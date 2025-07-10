8b6259ec75eeab24a990c6f324e18ca46c179eaf
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-07-11 00:15:01 +0530

    Added Sub-Child Div

--- ReactCDN/app.js
+++ ReactCDN/app.js
@@ -8,8 +8,19 @@ console.log(heading); // returns an object
 console.log(heading.type); // returns the type of the element, in this case "
 
 const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child"}, "Child H1"),
-    "Parent"
+    [       
+        React.createElement("div", { id: "child"},
+            [
+            React.createElement("div", { id: "child"}, "Child H1 - CHILD"),
+            React.createElement("div", { id: "child"}, "Child H2 - CHILD"),
+            ],
+            "Child H1 - PARENT"
+        ),
+        React.createElement("div", { id: "child"}, "Child H2"),
+        React.createElement("div", { id: "child"}, "Child H3")
+    ],
+    "PARENT DIV"
 );
+    
 const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 root2.render(parent);
