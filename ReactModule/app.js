d3651e6f6c0844b2af2a15f9ab01de1451ab8126
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-08-20 23:16:34 +0530

    Learned About JSX, Babel, and Multi-Line JSX

--- ReactModule/app.js
+++ ReactModule/app.js
@@ -1,27 +1,16 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World");
+const heading = React.createElement("h1", {id:"heading"}, "Hello World");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
 
-console.log(heading);       // returns an object
-console.log(heading.type);  // returns the type of the element, in this case "h1"
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { key: "child1", id: "child" }, 
-        [
-        React.createElement("div", { key: "child1a", id: "child" }, "Child H1 - CHILD"),
-        React.createElement("h1", { key: "child1b", id: "child" }, "Child H2 - CHILD"),
-        ],
-        "Child H1 - PARENT"
-    ),
-    React.createElement("div", { key: "child2", id: "child" }, "Child H2"),
-    React.createElement("div", { key: "child3", id: "child" }, "Child H3")
-    ],
-    "PARENT DIV"
-);
+const jsxHeading = <h1 id ="heading" className="hi"> This is a JSX syntaxx </h1> 
+//Browser does not recognise this code
+//Browser only understand pure javascript code 
 
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
-root2.render(parent);
+
+root.render(jsxHeading);
+
+
+//JSX - is not HTML | HTML-Like or XML-Like syntaxx 
