636b7c9f096ae68f488cf52ada4fdbdce175e5d0
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-08-21 22:47:57 +0530

    Learned About React Element, Functional Component and the beauty of JSX

--- ReactModule/app.js
+++ ReactModule/app.js
@@ -1,16 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './app.css';
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World");
+// React Element
+const heading  = (
+    <h1 className="head" tabIndex={5}>
+        React Element 
+    </h1>
+);
+
+
+// Functional React Component
+const HeadingComponent = () => {
+    return <h1 className="heading"> React Functional Component </h1>;
+}
+
+const a = 1000000;
+// both the function does the same thing just the change in brackets and return keyword 
+const HeadingComponent2 = () => (
+    <div id = "container">
+        {a}
+        <h2>{100 + 200}</h2>
+        {console.log("asdssasd")}
+        {heading}               {/* react Element inside JSX */}
+        <HeadingComponent/>    {/* Component : Compositions : call one function inside other */}
+        <h1 className="heading"> React Functional Component 2 </h1>;
+    </div>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-const jsxHeading = <h1 id ="heading" className="hi"> This is a JSX syntaxx </h1> 
-//Browser does not recognise this code
-//Browser only understand pure javascript code 
-
-
-root.render(jsxHeading);
-
-
-//JSX - is not HTML | HTML-Like or XML-Like syntaxx 
+root.render(
+    <div>
+     <HeadingComponent2/>
+    </div>
+);
